a9b1c3ae980d496309c101ebba1290a8385daef3
Vegnil <vegard.nilsen@atea.no>
2016-11-03 15:49:40 +0100

    Update vnxsp.py

--- lib/pyVsphereInflux/vnxsp.py
+++ lib/pyVsphereInflux/vnxsp.py
@@ -78,7 +78,7 @@ def build_vnxsp(vnx, tags, fields, measurement='vnxprop', args=None):
 
     for data in recs:
         missing_data = False
-        meas = "%s.%s" % test)
+        meas = "%s.%s" % (measurement)
         ts = InfluxResult(meas)
         for tag in tags:
             try:
