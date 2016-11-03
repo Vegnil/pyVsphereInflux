9e231d02e0102a4a8edfcd0ab7ce49bc355f1a1d
Vegnil <vegard.nilsen@atea.no>
2016-11-03 10:16:52 +0100

    Update vnx-sp-influxdb-import.py

--- bin/vnx-sp-influxdb-import.py
+++ bin/vnx-sp-influxdb-import.py
@@ -33,7 +33,7 @@ def main():
     for vnx in args.vnx:
 
         meas = "vnxprop.%s" % (convert_to_alnum(vnx))
-        results = build_vnx(vnx, vnx_tags, vnx_fields, 
+        results = build_vnxsp(vnx, vnx_tags, vnx_fields, 
                             measurement=meas, args=args)
         
         if args.debug:
