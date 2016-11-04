6af32835c7679a5e915e215c462a02b44609d30c
Vegnil <vegard.nilsen@atea.no>
2016-11-04 08:18:49 +0100

    Update vnx-sp-influxdb-import.py

--- bin/vnx-sp-influxdb-import.py
+++ bin/vnx-sp-influxdb-import.py
@@ -10,7 +10,7 @@ from pyVsphereInflux.tools.regex import convert_to_alnum
 
 influx_dsn_default = "influxdb://root:root@localhost:8086/database"
 
-vnx_tags = "cpu_0"
+vnx_tags = ['Prct_Busy']
 vnx_fields = ['Prct_Busy',
              'Prct_Idle']
 def main():
